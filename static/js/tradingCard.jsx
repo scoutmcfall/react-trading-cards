9feb72ab7ea1cdd84ed97172685953d21973f2ac
Scout McFall <scoutmknz@gmail.com>
2021-09-23 23:53:22 +0000

    add two more cards to cards page

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -13,7 +13,7 @@ function TradingCard(props) {
     <div className="card">
       <h2>Name: {props.name}</h2>
       <img src={props.imgUrl} alt="profile" />
-      <h2>Skill: </h2>
+      <h2>Skill:{props.skill} </h2>
     </div>
   );
 }
@@ -31,4 +31,14 @@ ReactDOM.render(
 ReactDOM.render(
   <TradingCard name="Llambda" skill="knitting scarves" imgUrl="/static/img/llambda.jpg" />,
   document.querySelector('#llambda')
+);
+
+ReactDOM.render(
+  <TradingCard name="Yichi" skill="eating carrots" imgUrl="/static/img/seedpy.jpeg" />,
+  document.querySelector('#yichi')
+);
+
+ReactDOM.render(
+  <TradingCard name="Scout" skill="eating cabbage" imgUrl="/static/img/shortstack-overflow.jpeg" />,
+  document.querySelector('#scout')
 );
